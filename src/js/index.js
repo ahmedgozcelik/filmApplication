ecf54e16b7264c812912aed8c6075e629eae18de
Ahmed Gürkan Özçelik <ahmetgozcelik@gmail.com>
2023-01-21 18:55:19 +0300

    The search model has been added to the application.

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,10 @@
 // url: https://api.themoviedb.org/3
 // https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
 
-async function getResult(keyword){
-    const api_key = "02f36b989cc341b1d6a1831b92e8a190";
-    const base_url = "https://api.themoviedb.org/3";
+//model-view-controller
 
-    const response = await fetch(`${base_url}/search/movie?api_key=${api_key}&page=1&query=${keyword}`);
-    const data = await response.json();
-    console.log(data.results);
-}
+import Search from './models/Search';
 
-getResult('abc');
+const search = new Search('abc');
+console.log(search);
+search.getResult();
